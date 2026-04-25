c3964bebe2366c76edec3024e94eda07f115b3f4
iriaceballos <iria.ceballos@gmail.com>
2026-04-25 18:32:45 +0200

    Create person_detect.py

---
+++ cuatroenraya/python/person_detect.py
@@ -0,0 +1,39 @@
+from arduino.app_bricks.video_objectdetection import VideoObjectDetection
+
+# Carrega el vostre model Edge Impulse local
+detection_stream = VideoObjectDetection(
+    model_path="./detector_persona.eim",
+    confidence=0.5,
+    debounce_sec=2.0
+)
+
+# variable global
+person_detected = False
+
+
+def _person_callback():
+    global person_detected
+    person_detected = True
+    print("Person detected")
+
+
+def initialize_detection():
+    """
+    Inicialitza el detector i registra el callback
+    """
+    detection_stream.on_detect("person", _person_callback)
+
+
+def detect_person():
+    """
+    Retorna True si s'ha detectat persona
+    Retorna False si no
+    """
+    global person_detected
+
+    current_state = person_detected
+
+    # reset pel següent cicle
+    person_detected = False
+
+    return current_state
