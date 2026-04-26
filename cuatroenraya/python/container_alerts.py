b1ac2211f1e898fd56b6af0f82627d870b5a8abd
janawpons <janaapons@gmail.com>
2026-04-26 05:34:39 +0200

    Add license and copyright comments to container_alerts.py
    Added licensing information and copyright notice.

--- cuatroenraya/python/container_alerts.py
+++ cuatroenraya/python/container_alerts.py
@@ -1,5 +1,10 @@
-FILL_THRESHOLD = 80
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Copyright (c) 2026 CuatroEnRaya Team
+
+FILL_THRESHOLD = 80
 
 def get_full_containers(containers):
     return [
